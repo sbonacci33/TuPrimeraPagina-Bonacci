06d893217f8637c2c61d6aa4cadc2caa28181107
Santiago Bonacci <sbonacci89@gmail.com>
2025-05-25 12:51:23 -0300

    Modelos creados y migraciones aplicadas: Categoria, Informe, ConsultaUsuario

--- observatorio/models.py
+++ observatorio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+    
+class Informe(models.Model):
+    titulo = models.CharField(max_length=200)
+    resumen = models.TextField()
+    contenido = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fuente = models.URLField(blank=True)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.titulo
+
+class ConsultaUsuario(models.Model):
+    termino_buscado = models.CharField(max_length=100)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.termino_buscado
