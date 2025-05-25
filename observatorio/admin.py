1a510286ff219bc55ae92fc1b9a5cfc14c354ebd
Santiago Bonacci <sbonacci89@gmail.com>
2025-05-25 13:01:26 -0300

    Modelos registrados en el admin y superusuario creado

--- observatorio/admin.py
+++ observatorio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Categoria, Informe, ConsultaUsuario
+
+admin.site.register(Categoria)
+admin.site.register(Informe)
+admin.site.register(ConsultaUsuario)
